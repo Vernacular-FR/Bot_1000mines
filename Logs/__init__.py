6d5d60695938c96966212c30f3176212087943f5
Vernacular-FR <robin.petitgerbier@gmail.com>
2025-12-10 05:19:22 +0100

    Delete Logs directory

--- Logs/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module d'initialisation du package Logs
-"""
-
-from .logger import GameLogger, get_logger, save_extraction_log, save_bot_log
-
-__all__ = ['GameLogger', 'get_logger', 'save_extraction_log', 'save_bot_log']
